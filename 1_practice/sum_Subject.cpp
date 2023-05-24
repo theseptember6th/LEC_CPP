e543b87ca747eafd000f3dc1a090530d27788b5d
Kristal Shrestha <shresthakristal42@gmail.com>
2023-05-24 17:09:43 +0545

    CHAPT 1

--- 1_practice/sum_Subject.cpp
+++ 1_practice/sum_Subject.cpp
@@ -5,17 +5,20 @@ int main()
     string subject[]={"OOP","TOC","ECT","EDC","DL","EM","MATH"};
     int marks[7];
     int i;
-    for(i=0;i<sizeof(marks)/sizeof(marks[0]);i++)
+    for(i=0;i<sizeof(subject)/sizeof(subject[0]);i++)
     {
         cout<<"enter the marks for "<<subject[i]<<"\n";
         cin>>marks[i];
     }
     int sum=0;
    
-   for(i=0;i<6;i++)
+   for(i=0;i<sizeof(marks)/sizeof(marks[0]);i++)
    {
     sum=sum+marks[i];
    }
-   cout<<"the total marks is "<<sum;
+   cout<<"the total marks is "<<sum<<" out of 700\n";
+//    double percentage;
+//    percentage=(sum/700)*100;
+//    cout<<"the percentage you got is "<<percentage<<"%";
 }      
 //THE END 
