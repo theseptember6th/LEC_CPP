f9597fe26b8ed1614c3f17cce514ebb4d2f9b98a
Kristal Shrestha <shresthakristal42@gmail.com>
2023-07-02 18:57:51 +0545

    nested namespace

--- self_study/4.cpp
+++ self_study/4.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 namespace outer{
     int num=100;
-    //cout<<num;             // this is error  why??
+    //cout<<num;             // why cant cout this is error  why??
     void display(){
-    cout<<num;              // forced to do this
+    cout<<num;              // but cout available if i make function
     }
     namespace inner{
         //num=10;              // this is error why?
@@ -18,11 +18,22 @@ namespace outer{
     }
 }
 
-using namespace outer::inner;
+ using namespace outer::inner;
 
 int main(){
-    cout<<num;
+    cout<<"\n"<<num; //inner ko num printed
+    num=num+30;// modifying num of inner
+     cout<<"\n"<<num;     // ye part haru ma maile directly num lekhna milyo because
+                          //yo inner namespace ko nai ho vanera mathi vaneko xa
+
+
     using namespace outer;
-    cout<<"\n"<<outer::num;
+    cout<<"\n"<<outer::num; //outer ko num printed
+    outer::num=outer::num+100; //modifying outer ko num
+    cout<<"\n"<<outer::num;  // tara ye part haru ma directly num lekhna milena becoz
+                             // maile mathi using namespace outer::inner garesake..so inner
+                    // ko ni huna sakcha ..but if maile mathi ko using namespace outer::inner
+                    //nalekheko vaye..yesma ni directly num lekhna milthyo becoz inner namespace
+                    //visible hudaina thyo...outer ko num matra visible hunthyo..
     return 0;
 }
