a2008f3e03592481a524ab3455d9b9e6c4eb03c1
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-16 14:57:44 +0545

    friend PREFIX NO RETURN

---
+++ C++_practice/OP_OV_practice/Unary_post_prefix/FRIEND FUNCTION/1.cpp
@@ -0,0 +1,27 @@
+//PREFIX
+// FRIEND Non member function no return 
+#include<iostream>
+using std::cin;
+using std::cout;
+
+class test{
+    int count;
+    public:
+    test(){
+        count=0;
+    } 
+    void display(){
+        cout<<count;
+    }
+  friend void operator++(test&);
+};
+void operator++(test&a){
+   a.count++;
+}
+int main(){
+    test c1;
+    ++c1;
+    c1.display();
+   
+return 0;
+}
