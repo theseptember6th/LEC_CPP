a60a1118059a30f4fc379f31aff163deb65df83f
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-16 14:49:53 +0545

    POSTFIX Non member funcn Yes Return

---
+++ C++_practice/OP_OV_practice/Unary_post_prefix/NON MEMBER FUNCTION/4.cpp
@@ -0,0 +1,32 @@
+//POSTFIX
+//Non member function yes return 
+#include<iostream>
+using std::cin;
+using std::cout;
+
+class test{
+    int count;
+    public:
+    test(){
+        count=0;
+    } 
+    void display(){
+        cout<<count;
+    }
+   int get_count(){
+    return count++;
+   }
+
+};
+test operator++(test&a,int){
+     a.get_count();
+     return a;
+    
+}
+int main(){
+    test c1;
+    c1++;
+    c1.display();
+   
+return 0;
+}
