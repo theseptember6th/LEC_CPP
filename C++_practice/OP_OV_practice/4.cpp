ca35d92574899289aadb0c41be6adec136833a43
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-16 12:35:55 +0545

    PREFIX MEMBER FUNCN USING CONSTRUCTOR

---
+++ C++_practice/OP_OV_practice/4.cpp
@@ -0,0 +1,30 @@
+//PRETFIX
+//member function YES return  using this parameterized constructor or returning nameless object
+#include<iostream>
+using std::cin;
+using std::cout;
+
+class test{
+    int count;
+    public:
+    test(){
+        count=0;
+    } 
+    test(int n){
+        count=n;
+    }
+    void display(){
+        cout<<count;
+    }
+    test operator ++(){
+        return test(++count);
+    }
+
+};
+int main(){
+    test c1;
+    ++c1;
+    c1.display();
+   
+return 0;
+}
