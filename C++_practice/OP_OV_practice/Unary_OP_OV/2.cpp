f7ecccdeffe942d48aff27508204990e0b73ba06
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-16 12:31:19 +0545

    Postfix member function no return

---
+++ C++_practice/OP_OV_practice/Unary_OP_OV/2.cpp
@@ -0,0 +1,27 @@
+//POSTFIX
+//member function no return
+#include<iostream>
+using std::cin;
+using std::cout;
+
+class test{
+    int count;
+    public:
+    test(){
+        count=0;
+    } 
+    void display(){
+        cout<<count;
+    }
+    void operator ++(int){
+        count++;
+    }
+
+};
+int main(){
+    test c1;
+    c1++;
+    c1.display();
+   
+return 0;
+}
