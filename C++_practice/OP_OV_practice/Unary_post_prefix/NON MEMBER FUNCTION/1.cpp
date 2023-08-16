b7998ac8bb9173c3e4ebc0cc11118bc1fb80faf3
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-16 13:40:24 +0545

    UNARY NON MEMBER NO RETURN

---
+++ C++_practice/OP_OV_practice/Unary_post_prefix/NON MEMBER FUNCTION/1.cpp
@@ -0,0 +1,30 @@
+//PREFIX
+// Non member function no return 
+#include<iostream>
+using std::cin;
+using std::cout;
+
+class test{
+    int count;
+    public:
+    test(){
+        count=0;
+    } 
+    void display(){
+        cout<<count;
+    }
+   int get_count(){
+    return ++count;
+   }
+
+};
+void operator++(test&a){
+    a.get_count();
+}
+int main(){
+    test c1;
+    ++c1;
+    c1.display();
+   
+return 0;
+}
