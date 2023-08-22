48ab323bd03e5c0799427feea18616626154e949
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-22 11:57:32 +0545

    Template 1

---
+++ C++_practice/TEMPLATE/1.cpp
@@ -0,0 +1,20 @@
+// one argument template
+#include<iostream>
+using std::cin;
+using std::cout;
+  template <class T>  // template and class are keywords
+T max(T a, T b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+int main(){
+    int x=10,y=20;
+    cout<<" "<<max(x,y)<<"\n";
+    float a=1.1,b=0.1;
+    cout<<" "<<max(a,b);
+return 0;
+}
