53be37d2ec960a3cf6cc33451afcfac4c3b89073
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-28 12:46:37 +0545

    fun program for write()fun

---
+++ C++_practice/STREAM COMPUTATION/2.cpp
@@ -0,0 +1,16 @@
+// write() function
+#include<iostream>
+#include<cstring>
+using std::cin;
+using std::cout;
+
+
+int main(){
+    char str[]="KRISTAL SHRESTHA ";
+    for(int i=0;i<strlen(str);++i){
+        cout.write(str,i);
+        cout<<"\n";
+    }
+    return 0;
+return 0;
+}
