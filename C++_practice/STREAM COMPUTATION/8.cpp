62d44f9ff6ddb69546d947553f1d4e9b8e46c8d9
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-31 08:07:44 +0545

    Standard manipulators

---
+++ C++_practice/STREAM COMPUTATION/8.cpp
@@ -0,0 +1,29 @@
+// parameterized manipulators
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+
+//setw(int)
+//cout<<5<<setw(6)<<123.4;     
+
+//setfill()
+//cout<<setw(5)<<setfill('#')<<5<<"\n";     
+
+//setprecision(int)
+// double x=12245.232;
+// cout<<setprecision(6)<<x;
+
+//setbase(int)
+// int x=255;
+// cout<<setbase(16)<<x; //change to hexa
+
+//setiosflags(long)
+
+// int x=5789;
+// cout<<setw(5)<<setiosflags(ios::hex|ios::left)<<x;     // code not displayed as commanded
+
+return 0;
+}
