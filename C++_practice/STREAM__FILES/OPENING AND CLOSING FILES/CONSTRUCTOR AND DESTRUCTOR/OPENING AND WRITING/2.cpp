d1e2c6efc3096622980cd79038b76d60f3c56a9e
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-31 12:05:24 +0545

    READING FROM FILE

---
+++ C++_practice/STREAM__FILES/OPENING AND CLOSING FILES/CONSTRUCTOR AND DESTRUCTOR/OPENING AND WRITING/2.cpp
@@ -0,0 +1,15 @@
+// reading from file 
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+   ifstream infile("contact.doc");
+   char name[25],email[25];
+   infile>>name;
+   infile>>email;
+   cout<<"data in my files are\n";
+   cout<<name<<email;
+return 0;
+}
