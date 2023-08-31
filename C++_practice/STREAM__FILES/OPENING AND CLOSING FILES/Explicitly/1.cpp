a9036ff361cc257bb826ed5cdd71b56459eaaad3
Kristal Shrestha <shresthakristal42@gmail.com>
2023-08-31 12:56:46 +0545

    writing to a file

---
+++ C++_practice/STREAM__FILES/OPENING AND CLOSING FILES/Explicitly/1.cpp
@@ -0,0 +1,17 @@
+// writing to a file
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+    char name[25],email[25];
+    ofstream outfile;
+    outfile.open("contact.doc");
+    cout<<"enter name:\n";
+    cin>>name;
+    cout<<"enter email:\n";
+    cin>>email;
+    outfile<<name<<endl;
+    outfile<<email;
+return 0;
+}
